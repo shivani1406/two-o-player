b378aea629527a93a9b370f25e5907ca6d6afdff
Shivani Sharma <shivani1406sharma@gmail.com>
2022-01-31 20:27:43 -0500

    initial commit

---
+++ two-o-player.rb
@@ -0,0 +1,19 @@
+class game 
+  attr_reader :player1, :player2, :result
+  attr_writer :player1, :player2, :result
+
+  def initialize
+    @player1 = 3
+    @player2 = 3
+  end
+
+  def generate_ques
+
+  end
+
+  def result
+
+  end
+
+
+end
